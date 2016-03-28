32f8a825d16c9465bc64380ee458c48ed33b0bfe
Catherine Karra and John Dow <john@>
2016-03-28 13:11:13 -0700

    We debugged our code that was returning an index rather than an array, so now our code works

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
     var sq2 = $("input#sq2").val();
     var sq3 = $("input#sq3").val();
     var arrayInput = [sq1, sq2, sq3];
-
+    arrayInput.push(sq2, sq1, sq3);
+    arrayInput.splice(-2, 3);
     $("#response1").text(arrayInput[1]);
     $("#response2").text(arrayInput[0]);
     $("#response3").text(arrayInput[2]);
